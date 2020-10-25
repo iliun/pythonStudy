80885504d87e7fea0adf38216f3201d6f248c48d
liun <liun@liundeMacBook-Pro.local>
2020-10-25 17:56:53 +0800

    mergeDict

--- mergeDict.py
+++ mergeDict.py
@@ -1,6 +1,11 @@
+from collections import ChainMap
 dic1 = {'x': 1, 'y': 2 }
 dic2 = {'y': 3, 'z': 4 }
+#修改merge1的value，dic1/dic2不会同步变化
+merge1 = {**dic1,**dic2}
 
-merged1 = {**dic1, **dic2}
+#修改merge2的value，dic1/dic2会同步变化
+merge2 = ChainMap(dic1,dic2)
 
-print(merged1)
+
+
