b1f7d3706572a68e68541467ce8da81d254374c5
liun <liun@liundeMBP.lan>
2020-12-19 19:11:21 +0800

    study1219

--- 1211.py
+++ 1211.py
@@ -133,6 +133,35 @@ def interview4():
         del l2[0]
     print(temp)
 
+def interview5():
+    #让所有奇数都在偶数前面，而且奇数升序排列，偶数降序排序，如字符串'1982376455',变成'1355798642'
+    l = '1235346250'
+    if isinstance(l,str):
+        l = [int(i) for i in l]
+    l.sort(reverse=True)
+    print(l)
+    for i in range(len(l)):
+        if l[i]%2 == 0:
+            l.insert(0,l.pop(i))
+    print(''.join(str(m) for m in l))
+
+def func1(l):
+    if isinstance(l, str):
+        l = [int(i) for i in l]
+    l.sort(reverse=True)
+    for i in range(len(l)):
+        if l[i] % 2 > 0:
+            l.insert(0, l.pop(i))
+    print(''.join(str(e) for e in l))
+
+def interview6():
+    a = 10
+    b = 20
+    c = [a]
+    a = 15
+    print(c)
+
+
 
 if __name__ == '__main__':
-    interview4()
+    interview6()
